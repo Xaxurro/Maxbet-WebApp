bc6417e1af6ff89c9a1c50ef7540445ff835e093
newuser161 <n4k4.1184@gmail.com>
2023-02-22 23:13:48 -0300

    Lag fix y autoformato

--- Front-End/src/Pages/Inventory.js
+++ Front-End/src/Pages/Inventory.js
@@ -3,15 +3,18 @@ import { ButtonFile } from "../Components/ButtonFile"
 import { Table } from "../Components/Table";
 import { Filter } from "../Components/Filter";
 import { Modal } from "../Components/Modal.js";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import "../Css/Modal.css"
 
 
-const Filters = ["Item id","Item Name","Origin","Owner Name","Status"]
-const Titles =[ {heading: 'Serial', value:"serial"},{heading: 'Item Name', value:"name"},{heading: 'Origin' , value:"origin"},{heading: 'Owner Name' , value:"owner"},{heading: 'Status' , value:"state"}];
-export function Inventory(){
+const Filters = ["Item id", "Item Name", "Origin", "Owner Name", "Status"]
+const Titles = [{ heading: 'Serial', value: "serial" }, { heading: 'Item Name', value: "name" }, { heading: 'Origin', value: "origin" }, { heading: 'Owner Name', value: "owner" }, { heading: 'Status', value: "state" }];
+const UPDATETIME = 60000;
+
+export function Inventory() {
     const [isAddModalActive, setAddModalState] = useState(false);
+    const [initDatos, setInitDatos] = useState(false);
     const [isUpdateModalActive, setUpdateModalState] = useState(false);
     const [items, setItems] = useState([]);
 
@@ -42,15 +45,16 @@ export function Inventory(){
         fetch('http://localhost:5000/product/', {
             method: 'GET',
         })
-        .then(response => response.json())
-        .then(json => {
-            if (json.success) return json.data;
-            return [];
-        })
-        .then(data => {
-            setItems(data);
-            return data;
-        });
+            .then(response => response.json())
+            .then(json => {
+                if (json.success) return json.data;
+                return [];
+            })
+            .then(data => {
+                console.log(data);
+                setItems([...data]);
+
+            });
     }
 
     const saveOne = () => {
@@ -72,7 +76,7 @@ export function Inventory(){
             },
             body: data
         })
-        .then(() => toggleAddModal());
+            .then(() => toggleAddModal());
     }
 
     const saveMore = () => {
@@ -94,7 +98,7 @@ export function Inventory(){
             },
             body: data
         })
-        .then(setISerial(""));
+            .then(setISerial(""));
     }
 
     const update = () => {
@@ -119,109 +123,131 @@ export function Inventory(){
             },
             body: data
         })
-        .then(() => toggleUpdateModal());
+            .then(() => toggleUpdateModal());
     }
 
     const deleteItem = serial => {
         const item = JSON.stringify({
             serial: serial
         });
-    
+
         fetch('http://localhost:5000/product/', {
-            headers:{          
+            headers: {
                 'Accept': 'application/json',
                 'Content-Type': 'application/json'
-            },          
+            },
             method: 'DELETE',
             body: item
         })
-        .then(() => toggleUpdateModal());
+            .then(() => toggleUpdateModal());
     }
 
-    const getName = e => {setIName(e.target.value)}
-    const getSerial = e => {setISerial(e.target.value)}
-    const getOrigin = e => {setIOrigin(e.target.value)}
-    const getOwner = e => {setIOwner(e.target.value)}
+    const getName = e => { setIName(e.target.value) }
+    const getSerial = e => { setISerial(e.target.value) }
+    const getOrigin = e => { setIOrigin(e.target.value) }
+    const getOwner = e => { setIOwner(e.target.value) }
 
-    getItems();
+    /**
+     * En la linea 17 se crea un hook que mantiene el estado de la inicializacion de los datos,
+     * que por defecto esta en false, esto es para que los datos se carguen inmediatamente al iniciar la app
+     * sino, habria que esperar la cantidad de milisegundos del UPDATETIME (60000ms | 1s).
+     * Los datos se continuan actualizando cada este intervalo mediante useEffect() para evitar
+     * que los datos se dupliquen exponencialmente (causa inicial del lag). Esto es porque el 
+     * arreglo vacio ([]) que se le pasa como segundo parametro a useEffect() le indica a esta 
+     * funcion que solo se ejecute una vez y no en cada renderizado
+     *  
+     */
+    if (!initDatos) {
+        getItems();
+        setInitDatos(true)
+    }
+    useEffect(() => {
+        setInterval(() => {
+            getItems();
+        }, UPDATETIME);
+    }, []);
+
+
+
+
 
     return (
-    <div className="Employees"> 
-        <div className="Title">
-            
-            <h1><i>Inventory</i></h1>
-            <div className="right">
-                <Filter data={Filters}/>
-                <Button className="Button" Text ="Search" onClick ={getItems}/>
-                <Button className="Button" Text ="Add Item" onClick ={toggleAddModal}/>
+        <div className="Employees">
+            <div className="Title">
+
+                <h1><i>Inventory</i></h1>
+                <div className="right">
+                    <Filter data={Filters} />
+                    <Button className="Button" Text="Search" onClick={getItems} />
+                    <Button className="Button" Text="Add Item" onClick={toggleAddModal} />
+                </div>
             </div>
-        </div>
-        <Modal State = {isUpdateModalActive} ChangeState = {toggleUpdateModal} Title = "Update Item">
-            <div className="ModalBody">
-                <div className="ModalRight">
-                    <label for="ISerial">Item Serial:</label><br/>
-                    <input id="ISerial" type="text" onChange={getSerial} value={ISerial}/><br/>
+            <Modal State={isUpdateModalActive} ChangeState={toggleUpdateModal} Title="Update Item">
+                <div className="ModalBody">
+                    <div className="ModalRight">
+                        <label for="ISerial">Item Serial:</label><br />
+                        <input id="ISerial" type="text" onChange={getSerial} value={ISerial} /><br />
 
-                    <label for="IName">Item Name:</label><br/>
-                    <input id="IName" type="text" onChange={getName} value={IName}/><br/>
+                        <label for="IName">Item Name:</label><br />
+                        <input id="IName" type="text" onChange={getName} value={IName} /><br />
 
-                    <label for="IOrigin">Item Origin:</label><br/>
-                    <input id="IOrigin" type="text" onChange={getOrigin} value={IOrigin}/><br/>
+                        <label for="IOrigin">Item Origin:</label><br />
+                        <input id="IOrigin" type="text" onChange={getOrigin} value={IOrigin} /><br />
 
-                    <label for="IOwner">Item Owner:</label><br/>
-                    <input id="IOwner" type="text" onChange={getOwner} value={IOwner}/><br/>
+                        <label for="IOwner">Item Owner:</label><br />
+                        <input id="IOwner" type="text" onChange={getOwner} value={IOwner} /><br />
+                    </div>
+
+
+                    <div className="Left">
+                        <label for="ChooseFile">
+                            <ButtonFile id="ChooseFile" className="ButtonFile" type="file" accept="image/png, image/jpg, image/gif, image/jpeg" />
+                        </label>
+                    </div>
+                    <br />
+                    <br />
+                    <br />
+                    <br />
+                    <br />
+                    <Button className='Button' Text='Update Item' onClick={update}></Button>
+                    <Button className='Button' Text='Delete Item' onClick={() => deleteItem(OldSerial)} />
+                    <Button className='Button' Text='Cancel' onClick={toggleUpdateModal}></Button>
                 </div>
+            </Modal>
+            <Modal State={isAddModalActive} ChangeState={toggleAddModal} Title="Add Item">
+                <div className="ModalBody">
+                    <div className="ModalRight">
+                        <label for="ISerial">Item Serial:</label><br />
+                        <input id="ISerial" type="text" onChange={getSerial} /><br />
+
+                        <label for="IName">Item Name:</label><br />
+                        <input id="IName" type="text" onChange={getName} /><br />
+
+                        <label for="IOrigin">Item Origin:</label><br />
+                        <input id="IOrigin" type="text" onChange={getOrigin} /><br />
+
+                        <label for="IOwner">Item Owner:</label><br />
+                        <input id="IOwner" type="text" onChange={getOwner} /><br />
+                    </div>
 
 
-                <div className="Left">
-                <label for="ChooseFile">
-                    <ButtonFile id="ChooseFile" className="ButtonFile" type="file" accept="image/png, image/jpg, image/gif, image/jpeg"/>
-                </label>
+                    <div className="Left">
+                        <label for="ChooseFile">
+                            <ButtonFile id="ChooseFile" className="ButtonFile" type="file" accept="image/png, image/jpg, image/gif, image/jpeg" />
+                        </label>
+                    </div>
+                    <br />
+                    <br />
+                    <br />
+                    <br />
+                    <br />
+                    <Button className='Button' Text='Add OtherItem' onClick={saveMore}></Button>
+                    <Button className='Button' Text='Add Item' onClick={saveOne}></Button>
+                    <Button className='Button' Text='Cancel' onClick={toggleAddModal}></Button>
                 </div>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <Button className='Button' Text='Update Item' onClick={update}></Button>
-                <Button className='Button' Text='Delete Item' onClick={() => deleteItem(OldSerial)}/>
-                <Button className='Button' Text='Cancel' onClick={toggleUpdateModal}></Button>
-            </div>
-        </Modal>
-        <Modal State = {isAddModalActive} ChangeState = {toggleAddModal} Title = "Add Item">
-            <div className="ModalBody">
-                <div className="ModalRight">
-                    <label for="ISerial">Item Serial:</label><br/>
-                    <input id="ISerial" type="text" onChange={getSerial}/><br/>
-
-                    <label for="IName">Item Name:</label><br/>
-                    <input id="IName" type="text" onChange={getName}/><br/>
-
-                    <label for="IOrigin">Item Origin:</label><br/>
-                    <input id="IOrigin" type="text" onChange={getOrigin}/><br/>
-
-                    <label for="IOwner">Item Owner:</label><br/>
-                    <input id="IOwner" type="text" onChange={getOwner}/><br/>
-                </div>
+            </Modal>
+            <Table data={items} column={Titles} setModalData={setUpdateModalData} />
 
 
-                <div className="Left">
-                <label for="ChooseFile">
-                    <ButtonFile id="ChooseFile" className="ButtonFile" type="file" accept="image/png, image/jpg, image/gif, image/jpeg"/>
-                </label>
-                </div>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <br/>
-                <Button className='Button' Text='Add OtherItem' onClick={saveMore}></Button>
-                <Button className='Button' Text='Add Item' onClick={saveOne}></Button>
-                <Button className='Button' Text='Cancel' onClick={toggleAddModal}></Button>
-            </div>
-        </Modal>
-        <Table data={items} column={Titles} setModalData={setUpdateModalData}/>
-        
-        
-    </div>);
+        </div>);
 }
